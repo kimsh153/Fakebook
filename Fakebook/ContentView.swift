5e1882b1e23117ffa8010b6e43c7fd8c2c515cc3
kimsh153 <sunghun4777@gmail.com>
2021-11-05 15:07:06 +0900

    Initial Commit

---
+++ Fakebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fakebook
+//
+//  Created by gsm10 on 2021/11/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fakebook/FakebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakebookApp.swift
+//  Fakebook
+//
+//  Created by gsm10 on 2021/11/05.
+//
+
+import SwiftUI
+
+@main
+struct FakebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
